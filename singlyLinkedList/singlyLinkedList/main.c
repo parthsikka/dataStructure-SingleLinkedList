0f0b8420ebb1699d3e3637cfa8d11b3a0fb7d12a
Parth Sikka <parthsikkah704@gmail.com>
2019-11-23 12:56:20 +0530

    Linked List Insertion

---
+++ singlyLinkedList/singlyLinkedList/main.c
@@ -0,0 +1,65 @@
+//
+//  main.c
+//  singlyLinkedList
+//
+//  Created by Parth Sikka on 23/11/19.
+//  Copyright © 2019 Parth Sikka. All rights reserved.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<curses.h>
+
+struct node
+{
+    int data ;
+    struct node* link ;
+} ;
+
+void push(struct node** head_ref, int new_data)
+{
+    //Step 1 : allocate memmory to the node :
+    struct node* new_node= (struct node*) malloc(sizeof(struct node)) ;
+    
+    //Step 2 : Add data to the next node ;
+    new_node->data = new_data ;
+    
+    //Step 3 : point this node to the next node ;
+    new_node -> link = (*head_ref) ;
+    
+    // Step 4: Point head to the new node
+    (*head_ref) = new_node ;
+    
+}
+
+void inserAfter(struct node* prev_node, int new_data)
+{
+    //Step 1 : To check if the previous node is not null
+ 
+    if (prev_node == NULL)
+    {    printf("The previous node can not be null") ;
+    return ;
+}
+    //Step 2 : create a new node :
+    struct node* new_node = (struct node*) malloc(sizeof(struct node)) ;
+    
+    //Step 3 : Add data to the new node again!
+    new_node -> data = new_data ;
+    
+    //Step 4 : Point this node to the next node :
+    new_node -> link = prev_node->link ;
+    
+    //Step 5 : Point prev node to this node :
+    prev_node->link = new_node ;
+    
+}
+
+int main()
+{
+    // This is a program for Linked List (singly)
+    
+    return 0 ;
+    
+}
+
+
